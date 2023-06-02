a2235d9f2eaa6705d2c8aa6280255f45c03dc216
jonyz1 <108366549+jonyz1@users.noreply.github.com>
2023-06-02 22:40:39 +0300

    Add files via upload

---
+++ flutter/packers_and_movers_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\tools\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Xeth\Documents\PROJECT\Y3 S2\Mobile Dev\Flutter\packers_and_movers_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
